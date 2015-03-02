d44fe91c7ef2d5ba6aeb0f151207ec8c879db694
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 05:26:21 +0000

    Tagging version 1.3.1
    git-svn-id: https://plugins.svn.wordpress.org/mw-google-maps/tags/1.3.1@1102994 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.mw-google-maps.js
+++ js/jquery.mw-google-maps.js
@@ -2,11 +2,11 @@
  * Name: jquery.mw-google-maps.js
  * Plugin URI: http://2inc.org/blog/category/products/wordpress_plugins/mw-google-maps/
  * Description: Google Maps API v3 操作
- * Version: 1.0
+ * Version: 1.2.1
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
- * Created: february 25, 2013
- * Modified:
+ * Created : August 28, 2013
+ * Modified: February 25, 2015
  * License: GPL2
  *
  * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
@@ -46,6 +46,7 @@
 ;( function( $ ) {
 
 	var plugname = 'mw_google_maps';
+	var useRoute = false;
 
 	var methods = {
 		/**
@@ -69,6 +70,7 @@
 				}
 			} );
 		},
+
 		/**
 		 * addMarker
 		 * マーカーの追加
@@ -85,6 +87,7 @@
 				data.points.push( $.extend( defaults, params ) );
 			} );
 		},
+
 		/**
 		 * render
 		 * レンダリング
@@ -102,8 +105,10 @@
 				// マップ表示
 				var map = new google.maps.Map( gmap.get( 0 ), {
 					center: position,
-					zoom: 13,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
+					zoom: data.config.zoom,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					scrollwheel: false,
+					scaleControl: true
 				} );
 				// マーカー設置
 				if ( data.points.length < 1 ||
@@ -111,11 +116,56 @@
 					 typeof( data.points[0].longitude ) == 'undefined' )
 					return true; // = jQuery's continue.
 
+				if ( useRoute === true ) {
+					// ルートを表示するマップを設定
+					var directionsRenderer = new google.maps.DirectionsRenderer();
+					directionsRenderer.setMap( map );
+
+					// 経由地点を設定（無料版は最大8箇所）
+					var wayPoints = [];
+					$.each( data.points, function( key, val ) {
+						wayPoints.push( {
+							location: new google.maps.LatLng(
+								val.latitude,
+								val.longitude
+							)
+						} );
+					} );
+
+					// 開始地点と終了地点、ルーティングの種類の設定
+					var request = {
+						origin: wayPoints[0].location,
+						destination: wayPoints.pop().location,
+						travelMode: google.maps.DirectionsTravelMode.DRIVING, // or BICYCLING, TRANSIT, WALKING
+						waypoints: wayPoints
+					};
+
+					// ルート検索を行う
+					var directionsService = new google.maps.DirectionsService();
+					directionsService.route( request, function( result, status ) {
+						if ( status == google.maps.DirectionsStatus.OK ) {
+							directionsRenderer.setDirections( result );
+							directionsRenderer.setOptions( {
+								suppressMarkers: true
+							} );
+						}
+					} );
+				}
+
+				// アイコン
+				var getIcon = function( key ) {
+					if ( useRoute === true ) {
+						return 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + ( key + 1 ) + '|F96757|000000';
+					}
+					return '';
+				}
+
 				var minLat = data.points[0].latitude;
 				var maxLat = data.points[0].latitude;
 				var minLng = data.points[0].longitude;
 				var maxLng = data.points[0].longitude;
 
+				var currentInfoWindow = null;
 				$.each( data.points, function( key, val ) {
 					var marker = new google.maps.Marker( {
 						position: new google.maps.LatLng(
@@ -123,7 +173,8 @@
 							val.longitude
 						),
 						draggable: val.draggable,
-						map: map
+						map: map,
+						icon: getIcon( key )
 					} );
 
 					if ( typeof( val.title != 'undefined' ) ) {
@@ -131,7 +182,11 @@
 							content: val.title
 						} );
 						google.maps.event.addListener( marker, 'click', function() {
+							if ( currentInfoWindow ) {
+								currentInfoWindow.close();
+							}
 							infowindow.open( map, marker );
+							currentInfoWindow = infowindow;
 						} );
 					}
 
@@ -164,6 +219,7 @@
 
 			} );
 		},
+
 		/**
 		 * geocode
 		 * geocode用のテキストフィールド等を定義
@@ -182,6 +238,14 @@
 				}
 				data.geocode = $.extend( defaults, params );
 			})
+		},
+
+		/**
+		 * useRoute
+		 * ルートサービスを使用する
+		 */
+		useRoute: function() {
+			useRoute = true;
 		}
 	};
 
@@ -191,10 +255,12 @@
 			 typeof( data.geocode.address ) !== 'undefined' &&
 			 typeof( data.geocode.latitude ) !== 'undefined' &&
 			 typeof( data.geocode.longitude ) !== 'undefined' &&
+			 typeof( data.geocode.zoom ) !== 'undefined' &&
 			 data.geocode.btn.length &&
 			 data.geocode.address.length &&
 			 data.geocode.latitude.length &&
 			 data.geocode.longitude.length &&
+			 data.geocode.zoom.length &&
 			 data.points.length == 1 )
 			)
 			return true;
@@ -226,6 +292,11 @@
 			map.setCenter( latlng );
 			data.geocode.address.val( '' );
 			setInputValueLatLng( event.latLng.lat(), event.latLng.lng() );
+		} );
+
+		google.maps.event.addListener( map, 'zoom_changed', function() {
+			var zoom = map.getZoom();
+			data.geocode.zoom.val( zoom );
 		} );
 
 		function setInputValueLatLng( lat, lng ) {
--- system/mw_google_maps_admin_page.php
+++ system/mw_google_maps_admin_page.php
@@ -3,14 +3,15 @@
  * Name: MW Google Maps Admin Page
  * Plugin URI: http://2inc.org/blog/category/products/wordpress_plugins/mw-google-maps/
  * Description: 管理画面クラス
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
- * Created: March 4, 2013
- * Modified:
+ * Created: february 25, 2013
+ * Modified: March 4, 2013
+ * Modified: February 25, 2015
  * License: GPL2
  *
- * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
+ * Copyright 2014 Takashi Kitajima (email : inc@2inc.org)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
@@ -29,12 +30,14 @@ class MW_Google_Maps_Admin_Page {
 
 	const NAME = 'mw-google-maps';
 	const DOMAIN = 'mw-google-maps';
+	private $level;
 	protected $option;
 
 	/**
 	 * __construct
 	 */
 	public function __construct() {
+		$this->level = 'manage_options';
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_init', array( $this, 'register_setting' ) );
 
@@ -62,7 +65,7 @@ class MW_Google_Maps_Admin_Page {
 	 */
 	public function view() {
 		if ( isset( $_POST['submit'] ) ) {
-			if ( ! current_user_can( 'manage_options' ) ) die( __( 'You cannot edit options.' ) );
+			if ( ! current_user_can( $this->level ) ) die( __( 'You cannot edit options.' ) );
 			$updateFlg = true;
 		}
 		?>
@@ -84,7 +87,7 @@ class MW_Google_Maps_Admin_Page {
 		// データベースから設定を取得
 		$option = get_option( self::NAME );
 		// 投稿タイプを取得
-		$post_types = get_post_types( array( 'public' => true ) );
+		$post_types = get_post_types( array( 'show_ui' => true ) );
 		unset( $post_types['attachment'] );
 		unset( $post_types['links'] );
 		?>
@@ -124,6 +127,7 @@ class MW_Google_Maps_Admin_Page {
 			'address'   => '',
 			'latitude'  => '',
 			'longitude' => '',
+			'zoom'      => 13,
 		), (array)$post_meta );
 		return $post_meta;
 	}
@@ -153,7 +157,7 @@ class MW_Google_Maps_Admin_Page {
 	 * 設定メニューにプラグインのサブメニューを追加する
 	 */
 	public function admin_menu() {
-		add_options_page( 'MW Google Maps', 'MW Google Maps', 'manage_options', __FILE__,  array( $this, 'admin_page' ) );
+		add_options_page( 'MW Google Maps', 'MW Google Maps', $this->level, __FILE__,  array( $this, 'admin_page' ) );
 	}
 
 	/**
@@ -217,6 +221,7 @@ class MW_Google_Maps_Admin_Page {
 		?>
 		<input type="hidden" name="<?php echo esc_attr( self::NAME ); ?>_nonce" value="<?php echo wp_create_nonce( self::NAME ); ?>" />
 		<div id="<?php echo $this->get_id( 'map' ); ?>" style="width:100%;height:200px;"></div>
+		<input type="hidden" id="<?php echo $this->get_id( 'zoom' ); ?>" name="<?php echo $this->get_name( 'zoom' ); ?>" value="<?php echo esc_attr( $post_meta['zoom'] ); ?>" />
 		<table border="0" cellpadding="0" cellspacing="5" style="width:100%">
 			<tr>
 				<td>
@@ -282,17 +287,20 @@ class MW_Google_Maps_Admin_Page {
 		?>
 		<script type="text/javascript">
 		jQuery( function( $ ) {
-			var gmap = $( '#<?php echo $this->get_id( "map" ); ?>' ).mw_google_maps();
+			var gmap = $( '#<?php echo $this->get_id( "map" ); ?>' ).mw_google_maps( {
+				zoom: <?php echo esc_js( $post_meta['zoom'] ); ?>
+			} );
 			gmap.mw_google_maps( 'addMarker', {
-				latitude : <?php echo ( empty( $post_meta['latitude'] ) ) ? '35.71012566481748' : $post_meta['latitude']; ?>,
-				longitude: <?php echo ( empty( $post_meta['longitude'] ) ) ? '139.81149673461914' : $post_meta['longitude']; ?>,
+				latitude : <?php echo ( empty( $post_meta['latitude'] ) ) ? '35.71012566481748' : esc_js( $post_meta['latitude'] ); ?>,
+				longitude: <?php echo ( empty( $post_meta['longitude'] ) ) ? '139.81149673461914' : esc_js( $post_meta['longitude'] ); ?>,
 				draggable: true
 			} );
 			gmap.mw_google_maps( 'geocode', {
 				btn      : $( '#<?php echo esc_attr( self::NAME ); ?>_mapbtn' ),
 				address  : $( '#<?php echo $this->get_id( "address" ); ?>' ),
 				latitude : $( '#<?php echo $this->get_id( "latitude" ); ?>' ),
-				longitude: $( '#<?php echo $this->get_id( "longitude" ); ?>' )
+				longitude: $( '#<?php echo $this->get_id( "longitude" ); ?>' ),
+				zoom     : $( '#<?php echo $this->get_id( "zoom" ); ?>' )
 			} );
 			gmap.mw_google_maps( 'render' );
 		} );
